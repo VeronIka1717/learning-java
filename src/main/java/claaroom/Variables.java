ff68daae907b86db38ec9dad6d273448a40c2b47
nikagrischuk <verongra971708@gmail.com>
2024-06-05 14:30:12 +0300

    [classroom/29052024]:Add Varibalse.java

---
+++ src/main/java/claaroom/Variables.java
@@ -0,0 +1,88 @@
+package claaroom;
+
+public class Variables {
+    public static void main(String[] args) {
+        //ONE-LINE COMMENT
+        /*
+        Multi-line comment
+        I can also write nere
+         */
+
+        //Category:Integral
+
+        //byte:
+        byte smallNumber = 120;
+        //RGB (Red Green Blue)
+        byte red = 120;
+        byte green = 45;
+        byte blue = 127;
+        System.out.println(blue);
+        //int(Integer)
+        //Employee ID:
+        int employeeId = 123456;
+
+        //Population count:
+        int populationofLatvia = 1811956;
+        System.out.println(populationofLatvia);
+
+        //short
+        //Game score;
+        short playerScore = 15000;
+        //Year  of birth;
+        short yearOfBirth = 1990;
+
+        //cgar(Character)
+        //Grade;
+        char grade = 'A';
+        //Initials
+        char firsInitial = 'N';
+        char lastInitial = 'G';
+
+
+        //Long
+        //Bank Account Balance;
+        long accountBalance = 10000000000l;
+        System.out.println(accountBalance);
+
+        //Java 7:
+        //Improves readability
+        long population = 1_000_000_000l;//1 bilion
+
+        //Avoiding mistake
+        int creditCardNumber = 1234_5678;
+
+        //doubl
+        //Pi
+        double piValue = Math.PI;
+        System.out.println(piValue);
+
+        //Another way
+        double pi = 3.4_15_92;
+
+        //Floating numbers:
+        //Price:
+        float price = 1_234_567_8998_98F;
+
+        //Double
+        //Financial Debt:
+        double financialDebt = 23_456_789_123.45555;
+
+        //Distance
+        double distanceToSun = 149_549_870_3; //in km;
+
+        //Boolean
+        //has_ or is prefix;
+        boolean isSummer = false;
+        boolean hasSon = false;
+
+        //String (we can save any tex in double quotes)
+        String firstName = "Veronika";
+        String lastName = "Griscuka";
+
+        //var
+        var myCurrentAge = 26;
+        var myFullName = "Veronika Griscuka";
+        
+
+    }
+}
